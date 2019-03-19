e06ffff6193279d45fe3b9cec6af721d77910523
Andrea Bellandi <andrea.bellandi@desy.de>
2019-03-19 11:57:01 +0100

    removed gencore directory from .gitignore (mistake)

---
+++ src/gencore/mainwrap.cpp
@@ -0,0 +1,22 @@
+
+
+#include "genesis.h"
+#include <mpi.h>
+
+// very basic wrapper for genesis. Most of the genesis stuff is moved into genmain.
+
+int main (int argc, char *argv[]) {
+
+    //-------------------------------------------------------
+    // init MPI and get size etc.
+    //
+
+    MPI_Init(&argc, &argv); //MPI
+    string filename (argv[argc-1]);
+    string latname;
+    genmain(filename,latname,false,false,false);
+    MPI_Finalize(); // node turned off
+
+    return 0;
+
+}
